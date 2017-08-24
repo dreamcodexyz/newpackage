64e75fdb113c58bb9fc756c40c60cfe238a4ca66
Do Tu <tdng91@gmail.com>
2017-08-25 02:55:35 +0700

    update

---
+++ src/NewpackageServiceProvider.php
@@ -0,0 +1,27 @@
+<?php 
+namespace Dreamcode\Newpackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class NewpackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
